bfcff18dca2a3509493df13c627311b8202080d4
yaseminsalann <yaseminsalann@gmail.com>
2025-02-22 02:40:40 +0300

    AngryBirdsKlon
    AngryBirds oyunu giriş

---
+++ AngryBirdsKlon/AngryBirdsKlonTests/AngryBirdsKlonTests.swift
@@ -0,0 +1,17 @@
+//
+//  AngryBirdsKlonTests.swift
+//  AngryBirdsKlonTests
+//
+//  Created by Yasemin salan on 22.02.2025.
+//
+
+import Testing
+@testable import AngryBirdsKlon
+
+struct AngryBirdsKlonTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
